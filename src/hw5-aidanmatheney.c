6d34f5fdf3adaaec3e4f99ca01d67a74b2b4f5b0
Aidan Matheney <aidanmatheney@gmail.com>
2020-09-29 10:13:12 -0500

    Set up project

---
+++ src/hw5-aidanmatheney.c
@@ -0,0 +1,15 @@
+/*
+ * Aidan Matheney
+ * aidan.matheney@und.edu
+ *
+ * CSCI 451 HW5
+ */
+
+#include "../include/hw5.h"
+
+#include <stdlib.h>
+
+int main(int const argc, char ** const argv) {
+    hw5();
+    return EXIT_SUCCESS;
+}
---
+++ src/hw5.c
@@ -0,0 +1,10 @@
+#include "../include/hw5.h"
+
+#include <stdio.h>
+
+/**
+ * Run CSCI 451 HW5.
+ */
+void hw5(void) {
+    puts("HW5 Hello World");
+}
